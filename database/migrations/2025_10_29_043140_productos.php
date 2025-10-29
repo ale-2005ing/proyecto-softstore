ed8f3a5ae198ec67059bc8a9aa9fec707cdaafd5
ale2005 <dixonoliveros66@gmail.com>
2025-10-29 00:13:13 -0500

    arreglo de error que hizo jander y agregados los modelos para hacer los cruds

---
+++ database/migrations/2025_10_29_043140_productos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+          Schema::create('productos', function (Blueprint $table) {
+        $table->id();
+        $table->string('nombre');
+        $table->decimal('precio', 10, 2);
+        $table->integer('stock')->default(0);
+        $table->unsignedBigInteger('categoria_id')->nullable();
+        $table->unsignedBigInteger('proveedor_id')->nullable();
+        $table->integer('stock_min')->default(1);
+        $table->integer('stock_max')->default(9999);
+        $table->timestamps();
+
+        $table->foreign('categoria_id')->references('id')->on('categorias')->nullOnDelete();
+        $table->foreign('proveedor_id')->references('id')->on('proveedores')->nullOnDelete();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
---
+++ database/migrations/2025_10_29_043237_compras.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+            Schema::create('compras', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('proveedor_id');
+        $table->decimal('total', 10, 2);
+        $table->timestamps();
+
+        $table->foreign('proveedor_id')->references('id')->on('proveedores')->cascadeOnDelete();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('compras');
+    }
+};
